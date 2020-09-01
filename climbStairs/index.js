c9569e4989ee538eb67a6529cfced76ea267f0b6
Prince Dogra <princedogra.au@gmail.com>
2020-09-01 13:10:23 +1000

    climbStairs: updated

--- climbStairs/index.js
+++ climbStairs/index.js
@@ -8,10 +8,9 @@
  */
 
 export default n => {
-    if (n == 0 || n == 1) return 1;
-    if (n == 2) return 2;
 
     let ways = new Array(n+1);
+    ways[0] = 1;
     ways[1] = 1;
     ways[2] = 2;
 
